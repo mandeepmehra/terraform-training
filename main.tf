142303bd4a00edd7614cc3945987cfb196138767
Mandeep Mehra <mandeep.mehra@xebia.com>
2021-04-05 21:23:18 -0700

    Added variables

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
-provider "aws" {
-    region = "us-east-1"
-}
+
 
 resource "aws_instance" "myec2" {
-  ami = "ami-0742b4e673072066f"
+  count         = var.instanceCount
+  ami           = var.amiid
   instance_type = "t2.micro"
   tags = {
-    "Name" = "Mandeep-Instance"
+    "Name" = var.tags[count.index]
   }
 }
 
 output "myec2ipaddress" {
- value = aws_instance.myec2.public_ip
+  value = aws_instance.myec2[*].public_ip
 }
 
-
 output "myec2privateip" {
-    value = aws_instance.myec2.private_ip
+  value = aws_instance.myec2[*].private_ip
 }
